28e0d1ceae2e69527fd7214829fd7bb201a57063
gvteja99 <gvteja99@gmail.com>
2023-08-07 13:48:45 -0700

    Change port number in dockerfile

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Assigned PORT ${process.env.PORT}`)
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
