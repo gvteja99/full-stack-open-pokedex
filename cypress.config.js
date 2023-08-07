b125aa5e28ea5baed10f0d3ecaecc3144fbb0af3
gvteja99 <gvteja99@gmail.com>
2023-08-07 12:05:51 -0700

    Fix lint issues in 11.9 Simple end to end tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    /* eslint-disable no-unused-vars */
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
